5e0ede42b23106c3454a47de2d88eda75d708f58
Evelyn Li <evelyn.li@berkeley.edu>
2017-10-08 03:12:26 -0700

    locationGrader

--- src/locationGrader.java
+++ src/locationGrader.java
@@ -7,25 +7,29 @@ import java.util.*;
  * Created by Evelyn Li on 10/8/2017.
  */
 public class locationGrader {
+    static HashMap<String, LinkedList<Float>> locationGrading = new HashMap<>();
     public static void main(String[] args) {
         try{
             List<String> line = Files.readAllLines(Paths.get("locationGrading.txt"), StandardCharsets.UTF_8);
-            HashMap<String, LinkedList<Float>> locationGrading = new HashMap<>();
-            Scanner scanner = new Scanner(System.in);
+
             for(int i = 0; i<400; i++){
                 String[] row = line.get(i).split(",");
-                Float walkingGrade = Float.parseFloat(row[3]);
-                Float cyclingGrade = Float.parseFloat(row[4]);
                 LinkedList<Float> value = new LinkedList<>();
-                value.add(walkingGrade);
-                value.add(cyclingGrade);
+                value.add(Float.parseFloat(row[3]));
+                value.add(Float.parseFloat(row[4]));
                 locationGrading.put(row[1]+row[2], value);
-                System.out.print(row[1]+row[2]);
-                System.out.print(value);
-                System.out.println();
             }
         } catch (java.io.IOException e) {
             return;
+        }
+    }
+    public Float getLocationGrading(String origin, String destination, String transportationType){
+        if (transportationType.equals("walking")) {
+            return locationGrading.get(origin+destination).get(0);
+        } else if (transportationType.equals("cycling")) {
+            return locationGrading.get(origin+destination).get(1);
+        } else {
+            return 0.0f;
         }
     }
 
