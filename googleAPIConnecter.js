d02cc22b051e83f9a2c42cd8c2238049bae6569d
Evelyn Li <evelyn.li@berkeley.edu>
2017-10-07 19:55:23 -0700

    commit

---
+++ googleAPIConnecter.js
@@ -0,0 +1,32 @@
+var origins = []
+var destinations = [];
+
+var service = new google.maps.DistanceMatrixService();
+service.getDistanceMatrix(
+  {
+    origins: origins,
+    destinations: destinations,
+    travelMode: 'WALKING',
+  }, callback);
+
+function callback(response, status) {
+  Dictionary<(string,string), (int,int)> statistics;
+  
+  if (status == 'OK') {
+    var origins = response.originAddresses;
+    var destinations = response.destinationAddresses;
+
+    for (var i = 0; i < origins.length; i++) {
+      var results = response.rows[i].elements;
+      for (var j = 0; j < results.length; j++) {
+        var element = results[j];
+        var distance = element.distance.text;
+        var duration = element.duration.text;
+        var from = origins[i];
+        var to = destinations[j];
+        statistics[(from, to)] = (distance, duration)
+      }
+    }
+  }
+}
+key = "AIzaSyAmW4-mEG7XTF_cAMeRHK_0GZ9cxvLwQt4"
